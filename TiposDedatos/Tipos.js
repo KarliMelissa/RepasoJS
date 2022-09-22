6b94a4d2f8a3af6c6f8bfb16858c55b5fe769ab0
KarliMelissa <melisan423@gmail.com>
2022-09-21 22:46:12 -0500

    Repaso tiposDatos

---
+++ TiposDedatos/Tipos.js
@@ -0,0 +1,14 @@
+let Nombre = 'Karla'
+let edad = 23
+let EstadoCivil = true
+
+console.log(typeof(Nombre))
+console.log(typeof(EstadoCivil))
+console.log(typeof(edad));
+
+if (edad === '23') {
+    console.log('ver');
+}else{
+    console.log('falso');
+}
+/**el triple igual significa que tiene que ser estrictamente igual.... */
