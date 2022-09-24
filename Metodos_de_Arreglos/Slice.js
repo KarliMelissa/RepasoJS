23bf8a5aef64009fb1d2b9e6ef57047aa373f5a8
alex lopez <mclexis18@gmail.com>
2022-09-23 23:31:26 -0500

    repasos split

--- Metodos_de_Arreglos/Slice.js
+++ Metodos_de_Arreglos/Slice.js
@@ -1,14 +1,14 @@
 //Selecciona elementos, tiene dos argumentos (comienzo, termino)
 
 //declaramos un array
-const nombres=['Ana', 'Javier', 'Mary', 'Luis']
+const nombres=['Ana', 'Javier', 'Mary', 'Luis', 'andrea', 'karen', 'emmely']
 
 //declaramos otra variable
 //y solo quiero los nombres de Ana y Javier
 //Slice, no toma en cuenta la segunda posición por lo que se
 //debe poner la siguiente para tomar el segundo nombre 0 y 2
 
-const nombres2=nombres.slice(0,2);
+const nombres2=nombres.slice(3, 5);
 
 //Ahora si quiero imprimir los nombres Mary y Luis, las 
 //posiciones que debo poner son 2 y 4
--- Metodos_de_Arreglos/Split.js
+++ Metodos_de_Arreglos/Split.js
@@ -6,3 +6,57 @@ let string="Hola gente como les va"
 let stringPasadoAArray= string.split(" ")
 
 console.log(stringPasadoAArray);
+
+/**separar o cambiar una letra en especifico de una cadena string*/
+
+/**esta es la forma largaaaaa */
+let cadena =  'El niño ñoño'
+
+let nuevoarray = cadena.split("");
+
+function Macth(item) {
+    if (item != 'ñ') {
+        return item
+    }
+    return item = 'n'  
+}
+
+let nuevoarreglo = nuevoarray.map(Math);
+
+let cadenanueva = nuevoarreglo.join("")
+console.log(cadenanueva);
+
+/**aqui termina la forma larga */
+
+/**esta es la forma corta*/
+/**rgex */
+let cadenaremplazada = cadena.replace(/ñ/g, 'n')
+console.log(cadenaremplazada);
+
+
+// let macthes = nuevoarray.filter(a=> a === 'ñ');
+
+// console.log(macthes);
+
+// let nuevacadena= .replace('ñ', 'n');
+
+// let nuevacadena = cadena.replace('ñ', 'n');
+
+// console.log(nuevacadena);
+
+/**Spplit 
+ * podemos separar cada una de las palabras y las pone en un elemento de un array
+ */
+
+/**replace
+ * remplaza un caracter por otro en una cadena de texto
+ */
+
+/**join
+ * unir un array a string mediante un seperados example join("")
+ */
+
+/**map
+ * recorre un array pero se le pueden realizar iteraciones dentro
+ * y a su vez devuelve un nuevo sin modificar el principal 
+ */
